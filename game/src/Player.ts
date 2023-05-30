07d18c1b05cb8a96a6ba9d285342b72881fad5f6
Владислав Захаров <vladislavzakharov@MacBook-Air-Vladislav.local>
2023-05-30 22:11:50 +0530

    add collison

--- game/src/Player.ts
+++ game/src/Player.ts
@@ -1,10 +1,11 @@
+import {MAP} from "./constants";
 import {degreeToRadians} from "./utils";
-import {BUTTON_DOWN, BUTTON_LEFT, BUTTON_RIGHT, BUTTON_UP, GAMEPAD1} from "./wasm4";
+import {BUTTON_DOWN, BUTTON_LEFT, BUTTON_RIGHT, BUTTON_UP, GAMEPAD1, trace} from "./wasm4";
 
 export class Player {
   static fov: f32 = 60;
   static movement: f32 = 0.05;
-  static rotation: f32 = 1.0;
+  static rotation: f32 = 1.5;
 
   angle: f32 = 90;
   x: f32 = 2;
@@ -14,21 +15,30 @@ export class Player {
 
   update(): void {
     const gamepad = load<u8>(GAMEPAD1);
+    let updatedX: f32 = this.x;
+    let updatedY: f32 = this.y;
 
     if (gamepad & BUTTON_UP) {
       const playerCos = Math.cos(degreeToRadians(this.angle)) * Player.movement;
       const playerSin = Math.sin(degreeToRadians(this.angle)) * Player.movement;
-      this.x += f32(playerCos);
-      this.y += f32(playerSin);
+      updatedX += f32(playerCos);
+      updatedY += f32(playerSin);
     } else if (gamepad & BUTTON_DOWN) {
       const playerCos = Math.cos(degreeToRadians(this.angle)) * Player.movement;
       const playerSin = Math.sin(degreeToRadians(this.angle)) * Player.movement;
-      this.x -= f32(playerCos);
-      this.y -= f32(playerSin);
-    } else if (gamepad & BUTTON_LEFT) {
+      updatedX -= f32(playerCos);
+      updatedY -= f32(playerSin);
+    }
+
+    if (gamepad & BUTTON_LEFT) {
       this.angle -= Player.rotation;
     } else if (gamepad & BUTTON_RIGHT) {
       this.angle += Player.rotation;
+    }
+
+    if (MAP[i32(Math.floor(updatedY))][i32(Math.floor(updatedX))] === 0) {
+      this.x = updatedX;
+      this.y = updatedY;
     }
   }
 }
